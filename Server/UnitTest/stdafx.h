392700a0e9c47694c8382218de4b05bf2ce88fa1
ichenq <ichenq@gmail.com>
2013-12-31 15:23:03 +0800

    UnitTest的预编译头

--- Server/UnitTest/stdafx.h
+++ Server/UnitTest/stdafx.h
@@ -2,7 +2,7 @@
 
 #ifdef _WIN32
 #   ifndef _WIN32_WINNT
-#       define _WIN32_WINNT	    0x0502	// Windows 2003
+#       define _WIN32_WINNT	    0x0600	// Windows 2008
 #   endif
 #   ifndef WIN32_LEAN_AND_MEAN
 #       define WIN32_LEAN_AND_MEAN
@@ -10,8 +10,8 @@
 #endif
 
 #ifdef _MSC_VER
-#   if _MSC_VER < 1600
-#   error "You need Visual C++ 2010(or higher version) to compile this project."
+#   if _MSC_VER < 1700
+#   error "You need Visual C++ 2012(or higher version) to compile this project."
 #   endif
 #endif
 
