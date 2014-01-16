a32365aacaee8f181ba4f808ec37c32780c87302
ichenq <ichenq@gmail.com>
2014-01-16 19:55:30 +0800

    修改Random

--- Server/Utility/Random.h
+++ Server/Utility/Random.h
@@ -1,9 +1,18 @@
 /**
  * @file    Random.h
+ * @author  chenqiang01@7aurora.com
  * @date    Dec 23, 2013
- * @author  
- * @brief   A simple random number generator based on std::mt19937
+ * @brief   基于std::mt19937的随机数生成器
+ *          摘自: https://github.com/facebook/rocksdb/blob/master/util/random.h
  *
+ *          随机数生成器:
+ *              std::linear_congruential_engine    线性同余法
+ *              std::mersenne_twister_engine       梅森旋转法
+ *              std::substract_with_carry_engine   滞后Fibonacci
+ *
+ *          分布类型
+ *              std::uniform_int_distribution       整数均匀分布
+ *              std::uniform_real_distribution      浮点数均匀分布 
  */
 
 #pragma once
@@ -11,23 +20,12 @@
 #include <random>
 #include <cstdint>
 
-//
-// 随机数生成器:
-//      std::linear_congruential_engine，线性同余法
-//      std::mersenne_twister_engine，梅森旋转法
-//      std::substract_with_carry_engine，滞后Fibonacci
-// 
-//分布类型
-//      std::uniform_int_distribution， 整数均匀分布
-//      std::uniform_real_distribution，浮点数均匀分布
-// 
-
 // 每个线程使用一个Random对象产生随机数
 class Random 
 {
 public:
-    explicit Random(uint32_t seed = std::random_device()()) 
-        : generator_(seed) 
+    explicit Random() 
+        : generator_(std::random_device()()) 
     { }
     
     // Generates the next random number
@@ -35,6 +33,10 @@ public:
     { 
         return generator_(); 
     }
+    
+    // Randomly returns true ~"1/n" of the time, and false otherwise.
+    // REQUIRES: n > 0
+    bool OneIn(uint64_t n) { return Uniform(n) == 0; }
 
     // Returns a uniformly distributed value in the range [0..n-1]
     // REQUIRES: n > 0
