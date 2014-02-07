dc9f2abd58a2958b5ac11673ff9b525d97183820
ichenq <ichenq@gmail.com>
2014-02-07 19:08:35 +0800

    Server/UnitTest: 删除未使用组件的测试用例

--- Server/UnitTest/stdafx.h
+++ Server/UnitTest/stdafx.h
@@ -1,18 +1,15 @@
 #pragma once
 
 #ifdef _WIN32
-#   ifndef _WIN32_WINNT
-#       define _WIN32_WINNT	    0x0600	// Windows 2008
-#   endif
-#   ifndef WIN32_LEAN_AND_MEAN
-#       define WIN32_LEAN_AND_MEAN
-#   endif
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
 #endif
 
 #ifdef _MSC_VER
-#   if _MSC_VER < 1700
-#   error "You need Visual C++ 2012(or higher version) to compile this project."
-#   endif
+#if _MSC_VER < 1800
+#error "代码使用了C++11(variadic template, rvalue reference)，需要Visual C++ 2013或更高版本."
+#endif
 #endif
 
 #include <gtest/gtest.h>
